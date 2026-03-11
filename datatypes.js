fb61d58ef83248df689d4fcc6e9e46407b9eb80a
Likhitha140603 <likhilikhitha438@gmail.com>
2026-03-11 15:04:54 +0530

    js.1

---
+++ datatypes.js
@@ -0,0 +1,22 @@
+
+var age = 21;                
+var name = "Balaji";         
+var isStudent = true;        
+
+let marks = [80, 85, 90];   
+let city = "Chittoor";       
+
+const pi = 3.14159;          
+const person = {             
+  name: "Balaji",
+  age: 21
+};
+
+
+console.log("Age:", age);
+console.log("Name:", name);
+console.log("Is Student:", isStudent);
+console.log("Marks:", marks);
+console.log("City:", city);
+console.log("Pi Value:", pi);
+console.log("Person Object:", person);
